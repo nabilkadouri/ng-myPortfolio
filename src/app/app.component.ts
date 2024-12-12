699043c4de2d8b9e91256836cb45a5b0b789b745
NabilKADOURI <kadourinabil7@gmail.com>
2024-12-12 23:34:23 +0100

    add hero and conf tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';
 import { ProjectComponent } from './project/project.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { HardSkillComponent } from './hard-skill/hard-skill.component';
@@ -11,7 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,HeroComponent,ProjectComponent,ExperienceComponent,HardSkillComponent,SoftSkillComponent,HobbyComponent, FooterComponent],
+  imports: [HeaderComponent,ProjectComponent,ExperienceComponent,HardSkillComponent,SoftSkillComponent,HobbyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,24 @@ module.exports = {
     },
     extend: {
       colors:{
-      bleu : "#00AAE2",
-      nav: "#3A393C"
+        bleu : "#00AAE2",
+        nav: "#3A393C"
+      },
+      backgroundImage: {
+        degrader: 'radial-gradient(76.04% 106.72% at 88.26% 29.29%, rgba(255, 255, 255, 0) 37.85%, #000000 61.76%)',
+        'gradient-radial': 'radial-gradient(134% 134% at 50% 50%, rgba(140, 105, 52, 0) 0%, rgba(242, 181, 90, 0.8) 100%)',
+      },
+      dropShadow: {
+        'custom': '0px 0px 30px rgba(255, 215, 0, 0.25)',
+      },
+      textShadow: {
+        custom: '0px 4px 4px #EEAC63',
       },
     }
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('tailwindcss-textshadow')
   ]
 }
 
