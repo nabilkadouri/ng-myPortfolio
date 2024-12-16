3a56c83b99a2292d4a9ae3089d7df803646772fe
NabilKADOURI <kadourinabil7@gmail.com>
2024-12-16 14:42:45 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
 
 export const routes: Routes = [
-
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'accueil', component: AppComponent },
+  { path: '**', redirectTo: '/home' },
 ];
