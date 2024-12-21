19a4c10988735ffaa50cc4d858c257b2c7676f82
NabilKADOURI <kadourinabil7@gmail.com>
2024-12-21 14:30:03 +0100

    update error nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [HeroComponent],
+  imports: [HeroComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   // Variable pour gérer l'état du menu
   isMenuOpen: boolean = false;
+screenWidth: any;
 
   // Méthode pour ouvrir/fermer le menu
   toggleMenu(): void {
