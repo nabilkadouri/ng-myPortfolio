b238ce06a68e79c4fd766ee6d0b542f3489527a2
NabilKADOURI <kadourinabil7@gmail.com>
2024-12-22 22:03:58 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
-  { path: 'accueil', component: AppComponent },
-  { path: '**', redirectTo: '/home' },
+ 
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { CommonModule } from '@angular/common';
 
@@ -8,10 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   // Variable pour gérer l'état du menu
   isMenuOpen: boolean = false;
-screenWidth: any;
+  screenWidth: number = 0;
 
   // Méthode pour ouvrir/fermer le menu
   toggleMenu(): void {
@@ -21,5 +21,16 @@ screenWidth: any;
   // Méthode pour fermer le menu après un clic sur un lien
   closeMenu(): void {
     this.isMenuOpen = false;
+  }
+
+  // Récupère la largeur de l'écran au redimensionnement
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  // Initialise la largeur de l'écran au chargement
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
   }
 }
